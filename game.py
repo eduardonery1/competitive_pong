b246502e9cd4689ab2c03e80cd0cbef45bada55a
Eduardo Nery <eduardo.nery8@gmail.com>
2023-12-16 00:58:29 -0300

    basic game-user relationships

---
+++ game.py
@@ -0,0 +1,10 @@
+class Game:
+    def __init__(self, user1, user2):
+        self.user1 = user1
+        self.user2 = user2
+        self.user1.playing = True
+        self.user2.playing = True
+
+    def update(data):
+        await self.user1.send(data)
+        await self.user2.send(data)
---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, playing, pos):
+        self.playing = playing
+        self.pos = pos
+
+    
